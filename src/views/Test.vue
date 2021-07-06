905bc5734e85c620d78dc6f6c28eb27565ffe9bd
lsc <lishichao@soyiteck.com>
2021-07-06 17:19:09 +0800

    修复 bug

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,23 +2,18 @@
   <div class="w-full h-full box-border text-center">
     <div class="font-bold text-3xl p-5">Unit Test Page</div>
     <p class="mb-3 mt-3">count is: {{ count }}</p>
+    <p>{{ text }}</p>
     <button @click="increment">increment</button>
   </div>
 </template>
 
-<script lang="ts" type="javascript">
-import { defineComponent, ref } from 'vue'
-
-export default defineComponent({
-  name: 'Vuex',
-  setup() {
-    const count = ref<number>(0)
-    const increment = () => {
-      count.value += 1
-    }
-    return { count, increment }
-  }
-})
+<script lang="ts" type="javascript" setup>
+import { ref } from 'vue'
+const count = ref<number>(0)
+ref: text = '等待数据读取'
+const increment = () => {
+  count.value += 1
+}
 </script>
 
 <style scoped lang="scss">
